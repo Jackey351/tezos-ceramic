222b8ef12c107da5f831e287648e5a9c87e03b22
lhd791150351 <791150351@qq.com>
2023-01-29 17:34:45 +0800

    feat: update demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -5,9 +5,34 @@ import { DIDSession } from "did-session";
 import { TezosWebAuth, getAccountId } from "@didtools/pkh-tezos";
 import { DAppClient } from "@airgap/beacon-sdk";
 import { CeramicClient } from "@ceramicnetwork/http-client";
-import { TileDocument } from "@ceramicnetwork/stream-tile";
+import {
+  ModelInstanceDocument,
+  type ModelInstanceDocumentMetadataArgs,
+} from "@ceramicnetwork/stream-model-instance";
+import { StreamID } from "@ceramicnetwork/streamid";
 
 let tzProvider: DAppClient;
+
+export interface DataverseProfile {
+  name: string;
+  description: string;
+  image: {
+    original: {
+      src: string;
+      mimeType?: string;
+      width?: number;
+      height?: number;
+    };
+  };
+  background: {
+    original: {
+      src: string;
+      mimeType?: string;
+      width?: number;
+      height?: number;
+    };
+  };
+}
 
 function App() {
   const handleTest = async () => {
@@ -41,27 +66,58 @@ function App() {
     console.log({ accountId });
     const authMethod = await TezosWebAuth.getAuthMethod(tzProvider, accountId);
 
+    const profileModel =
+      "kjzl6hvfrbw6ca387paeel0z42xq5ivcrheiox3i25rp0hps15r4w12tvr2l939";
+
     const session = await DIDSession.authorize(authMethod, {
-      resources: ["ceramic://*"],
+      resources: [`ceramic://*?model=${profileModel}`],
     });
     console.log({ session });
-    console.log({ signature: session.cacao.s.s });
+    console.log({ signature: session.cacao.s?.s });
 
     // //前端模拟验证签名
     // await verifyTezosSignature(session.cacao, {});
     // console.log("signature verified");
 
-    const ceramic = new CeramicClient("http://127.0.0.1:7007");
+    const ceramic = new CeramicClient(
+      "https://local1.dataverseceramicdaemon.com"
+    );
     ceramic.did = session.did;
     console.log(ceramic);
 
-    const doc = await TileDocument.create(ceramic, { foo: "bar" });
-    console.log(doc);
-    console.log(doc.id.toString());
+    // const did = ceramic.did.parent;
 
-    const doc1 = await TileDocument.load(ceramic, doc.id.toString());
-    console.log(doc1);
-    console.log(doc1.state.content);
+    // if (did) midMetadataArgs["controller"] = did;
+
+    const doc = await ModelInstanceDocument.single(ceramic, {
+      model: StreamID.fromString(profileModel),
+    });
+    console.log(doc.content);
+
+    await doc.replace({
+      name: "test_name",
+      description: "test_description",
+      image: {
+        original: {
+          src: "https://i.seadn.io/gcs/files/4df295e05429b6e56e59504b7e9650b6.gif?w=500&auto=format",
+        },
+      },
+      background: {
+        original: {
+          src: "https://i.seadn.io/gae/97v7uBu0TGycl_CT73Wds8T22sqLZISSszf4f4mCrPEv5yOLn840HZU4cIyEc9WNpxXhjcyKSKdTuqH7svb3zBfl1ixVtX5Jtc3VzA?w=500&auto=format",
+        },
+      },
+    });
+
+    console.log(doc.content);
+
+    // const doc = await TileDocument.create(ceramic, { foo: "bar" });
+    // console.log(doc);
+    // console.log(doc.id.toString());
+
+    // const doc1 = await TileDocument.load(ceramic, doc.id.toString());
+    // console.log(doc1);
+    // console.log(doc1.state.content);
   };
 
   return (
