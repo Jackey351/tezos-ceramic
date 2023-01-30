1c139f490bdff698bf2e34703832212072116bb5
lhd791150351 <791150351@qq.com>
2023-01-30 11:47:28 +0800

    feat: use Temple to connect identity and create or update Dataverse profile

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,17 +1,11 @@
 import "./App.css";
 import "./App.css";
-
 import { DIDSession } from "did-session";
 import { TezosWebAuth, getAccountId } from "@didtools/pkh-tezos";
 import { DAppClient } from "@airgap/beacon-sdk";
 import { CeramicClient } from "@ceramicnetwork/http-client";
-import {
-  ModelInstanceDocument,
-  type ModelInstanceDocumentMetadataArgs,
-} from "@ceramicnetwork/stream-model-instance";
+import { ModelInstanceDocument } from "@ceramicnetwork/stream-model-instance";
 import { StreamID } from "@ceramicnetwork/streamid";
-import { ethers } from "ethers";
-import { login } from "./login";
 import { loadStreamsByModel } from "./api/loadStreamsByModel";
 
 let tzProvider: DAppClient;
@@ -39,73 +33,41 @@ export interface DataverseProfile {
 
 function App() {
   const handleTest = async () => {
-    // if (!tzProvider) {
-    //   tzProvider = new DAppClient({
-    //     name: "my dapp",
-    //   });
-    // }
-    // // await tzProvider.clearActiveAccount();
+    if (!tzProvider) {
+      tzProvider = new DAppClient({
+        name: "my dapp",
+      });
+    }
+    // await tzProvider.clearActiveAccount();
+    let activeAccount = await tzProvider.getActiveAccount();
+    let address;
+    let network;
+    if (activeAccount) {
+      address = activeAccount.address;
+      network = activeAccount.network.type;
+    } else {
+      const permissions = await tzProvider.requestPermissions();
+      console.log({ permissions });
+      address = permissions.address;
+      network = permissions.network.type;
+    }
 
-    // console.log({ tzProvider });
-
-    // let activeAccount = await tzProvider.getActiveAccount();
-    // let address;
-    // let network;
-    // console.log({ activeAccount });
-    // if (activeAccount) {
-    //   address = activeAccount.address;
-    //   network = activeAccount.network.type;
-    // } else {
-    //   const permissions = await tzProvider.requestPermissions();
-    //   console.log({ permissions });
-    //   address = permissions.address;
-    //   network = permissions.network.type;
-    // }
-
-    // console.log({ address });
-    // console.log({ network });
-
-    // const accountId = await getAccountId(tzProvider, address);
-    // console.log({ accountId });
-    // const authMethod = await TezosWebAuth.getAuthMethod(tzProvider, accountId);
+    const accountId = await getAccountId(tzProvider, address);
+    const authMethod = await TezosWebAuth.getAuthMethod(tzProvider, accountId);
 
     const profileModel =
       "kjzl6hvfrbw6ca387paeel0z42xq5ivcrheiox3i25rp0hps15r4w12tvr2l939";
 
-    // const session = await DIDSession.authorize(authMethod, {
-    //   resources: [`ceramic://*?model=${profileModel}`],
-    // });
-    // console.log({ session });
-    // console.log({ signature: session.cacao.s?.s });
+    const session = await DIDSession.authorize(authMethod, {
+      resources: [`ceramic://*?model=${profileModel}`],
+    });
 
-    // // //前端模拟验证签名
-    // // await verifyTezosSignature(session.cacao, {});
-    // // console.log("signature verified");
-
-    // const ceramic = new CeramicClient(
-    //   "https://local1.dataverseceramicdaemon.com"
-    // );
-    // ceramic.did = session.did;
-    // console.log(ceramic);
-
-    // const did = ceramic.did.parent;
-    // console.log(did);
-    // // if (did) midMetadataArgs["controller"] = did;
-
-    // const provider = ethers.getDefaultProvider();
-    // const signer = new ethers.Wallet(
-    //   "871cc2b0f1e92b81a5af21c744b9787d1cd2a67bbbe2606e2dcf71ab692120bc",
-    //   provider
-    // );
-    // const address = await signer.getAddress();
-    const { ceramic } = await login(
-      { resources: [`ceramic://*?model=${profileModel}`] }
-      // provider
+    const ceramic = new CeramicClient(
+      "https://local1.dataverseceramicdaemon.com"
     );
-    const did = ceramic.did!.parent;
-    console.log(ceramic);
+    ceramic.did = session.did;
 
-    // console.log(doc)
+    const did = ceramic.did.parent;
 
     const streams = await loadStreamsByModel({
       did,
@@ -151,17 +113,6 @@ function App() {
 
       console.log(doc.content);
     }
-
-    // console.log(doc.content);
-    // console.log(doc.content);
-
-    // const doc = await TileDocument.create(ceramic, { foo: "bar" });
-    // console.log(doc);
-    // console.log(doc.id.toString());
-
-    // const doc1 = await TileDocument.load(ceramic, doc.id.toString());
-    // console.log(doc1);
-    // console.log(doc1.state.content);
   };
 
   return (
